a6f2763d7f72ac5889b7ead12f66277257acf5c8
tokaretin <tokaretin@gmail.com>
2025-01-28 23:28:12 +0100

    5 module

---
+++ 5/5.2.1.py
@@ -0,0 +1,9 @@
+x = int(input('Введите координату X: '))
+y = int(input('Введите координату Y: '))
+
+if x > y:
+    print('X больше Y')
+elif y > x:
+    print('Y больше X')
+else:
+    print('X и Y равны')
