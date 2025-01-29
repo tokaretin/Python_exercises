ca9b6d2e4fc36cd3c060fb073f257ec5e86eabca
tokaretin <tokaretin@gmail.com>
2025-01-29 17:01:31 +0100

    I start working on the tasks

---
+++ module_5/5.6.py
@@ -0,0 +1,15 @@
+
+experience = int(input('Введите очки опыта: '))
+# Какое значение переменной level должно быть задано изначально?
+if experience < 0:
+    print('Ошибка')
+else:
+    if experience < 1000:
+        level = 1
+    elif experience < 2500:
+        level = 2
+    elif experience < 5000:
+        level = 3
+    else:
+        level = 4
+print(f'Уровень персонала: {level}')
