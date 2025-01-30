b56a240418595bb6805f04b6eaba4e78fd61e117
tokaretin <tokaretin@gmail.com>
2025-01-30 17:01:32 +0100

    I make 6 module

---
+++ module_6/6.5.1.py
@@ -0,0 +1,5 @@
+phrase = int(input('Введите сколько раз вывезти фразу "Я программист!": '))
+count = 0
+while count < phrase:
+    print('Я программист!')
+    count += 1
---
+++ module_6/6.5.2.py
@@ -0,0 +1,6 @@
+remember = int(input('Сколько раз вам напомнить? '))
+
+i = 0
+while i < remember:
+    print('Вы хотели не забыть о чём-то')
+    remember -= 1
