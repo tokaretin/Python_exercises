5635a8e419ad391368dd1de2851f22e142213d3a
tokaretin <tokaretin@gmail.com>
2025-01-31 20:34:04 +0100

    Binary Search Algorithm

---
+++ module_6/Practical_tasks_module_6/Binary Search Algorithm.py
@@ -0,0 +1,19 @@
+start = 1
+finish = 100
+count = 1
+
+while True:
+    n = (start + finish) // 2 # n = 50
+    print('Загаданное число равно, меньше или больше', n)
+    answer = int(input('1 - равно, 2 - меньше, 3 - больше '))
+
+    if answer == 2:
+        finish = n - 1 # 49
+    elif answer == 3:
+        start = n + 1
+    elif answer == 1:
+        print('Я угадал! Ура! с ', count, 'попытки')
+        break
+    count += 1
+
+print(f'Вы отгадали число с {count} попытки')
--- module_6/Practical_tasks_module_6/task_5.py
+++ module_6/Practical_tasks_module_6/task_5.py
@@ -1,4 +1,21 @@
 #Игра «Угадай число».
+number = int(input('Загадай число от 1 до 5: '))
+answer = 0
+number_of_attempts = 0
 
-a = 10
-print(a)
+
+while answer != number:
+    answer = int(input('Введите числ: '))
+    # Подсчет попыток
+    number_of_attempts += 1
+
+    if answer < number:
+        print('Число меньше, чем нужно. Попробуйте ещё раз! ')
+    elif answer > number:
+        print('Число больше, чем нужно. Попробуйте ещё раз! ')
+    else:
+        break
+
+
+print(f'Вы угадали! Число попыток: {number_of_attempts}')
+
