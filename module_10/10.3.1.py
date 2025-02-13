c6a2f2b53bb303d4916060503e26e91242873569
tokaretin <tokaretin@gmail.com>
2025-02-13 14:28:37 +0100

    Symbol Football Gate

---
+++ module_10/10.3.1.py
@@ -0,0 +1,21 @@
+# Задача 1. Врата
+# Напишите программу, которая выводит в консоль «врата»,
+# состоящие из тире, вертикальных линий и пробелов. Поле
+# состоит из 20 строк и 30 столбцов (но не стесняйтесь пробовать
+# и другие размеры).
+from html.parser import endendtag
+
+row_number = int(input('Введите высоту ворот: '))
+col_number = int(input('Введите ширину ворот: '))
+
+for row in range(row_number):
+    for col in range(col_number):
+        if row == 0:
+            print('-', end='')
+        elif col == 0:
+            print('|', end='')
+        elif col > col_number - 2:
+            print('|', end='')
+        else:
+            print(' ', end='')
+    print()
