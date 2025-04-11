599f51c8ddfe42f577b9682c4931a9fed8bce689
tokaretin <tokaretin@gmail.com>
2025-04-11 22:08:27 +0200

    task module 12

--- module_12/12.2.3.1_Post.py
+++ module_12/12.2.3.1_Post.py
@@ -1,6 +1,11 @@
-def post_name(name):
-    print('Ваще имя', name)
+def post_name(name, sername, house, apartment):
+    print('Ваще имя: ', name)
+    print('Ваще фамиоия: ', sername)
+    print('Ващ дом: ', house)
+    print('Ваша квартира: ', apartment, '\n')
 
-
-post_name('Илья')
+post_name('Илья', 'Каретин', '84', 32)
+post_name('Лев', 'Каретин', '83', 30)
+post_name('Симона', 'Каретина', '84', 32)
+post_name('Анастасия', 'Каретина', '84', 32)
 
